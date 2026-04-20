8004ff582645f556d106199ea0385b23f05c9f9d
doisonggio1 <taothien136@gmail.com>
2026-04-20 09:25:50 +0700

    first commit

---
+++ application/logs/log-2026-04-20.php
@@ -0,0 +1,28 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-04-20 09:05:48 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:05:48 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_exception.php): failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 219
+ERROR - 2026-04-20 09:05:48 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_exception.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 219
+ERROR - 2026-04-20 09:05:49 --> 404 Page Not Found: Faviconico/index
+ERROR - 2026-04-20 09:05:49 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_404.php): failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 182
+ERROR - 2026-04-20 09:05:49 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:05:49 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_404.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 182
+ERROR - 2026-04-20 09:05:49 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_exception.php): failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 219
+ERROR - 2026-04-20 09:05:49 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_exception.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 219
+ERROR - 2026-04-20 09:05:50 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:05:50 --> Severity: Warning --> include(/var/www/html/application/views/errors/html/error_exception.php): failed to open stream: No such file or directory /var/www/html/system/core/Exceptions.php 219
+ERROR - 2026-04-20 09:05:50 --> Severity: Warning --> include(): Failed opening '/var/www/html/application/views/errors/html/error_exception.php' for inclusion (include_path='.:/usr/local/lib/php') /var/www/html/system/core/Exceptions.php 219
+ERROR - 2026-04-20 09:07:00 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:01 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:01 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:01 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:01 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:01 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:02 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:02 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:02 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:03 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:03 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:03 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:07:03 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
+ERROR - 2026-04-20 09:17:32 --> Severity: error --> Exception: Class 'MY_Controller' not found /var/www/html/application/controllers/Home.php 4
